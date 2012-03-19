d946c9e75f972942c5613f870fa00c710ebbf95b
Chris Matthieu <chris@matthieu.us>
2012-03-18 21:12:38 -0700

    added admin support

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -93,6 +93,7 @@ class SessionsController < ApplicationController
       session['fullname'] = @authhash[:name]
       session['photo'] = @authhash[:photo]
       session['access_token'] = @authhash[:token]  
+      session['admin'] = @auth.admin 
         
       redirect_to "/"
         
